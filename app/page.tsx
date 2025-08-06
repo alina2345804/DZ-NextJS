1ca7eec78c0da7b20454fa24a639ca749316f2ce
Alina <alina2345nakataclovo@gmail.com>
2025-08-06 12:32:48 +0800

    10.11 ДЗ CSS Grid

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,42 @@
 'use client'
-import {Card, Button} from "@/components";
+import {Card} from '../components';
 
 export default function Home() {
-  return (
-    <>
-     <Card
-               category="Front-end"
-               point="·"
-               date="1 месяц назад"
-               title="Как работать с CSS Grid"
-               description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
-               readTime="3 минуты"
-               likeCount={4}
-               isLiked={false}
-           />
+    return (
         <>
-            <Button isActive={false} />
+            <Card
+                category="Front-end"
+                point="·"
+                date="1 месяц назад"
+                title="Как работать с CSS Grid"
+                description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+                readTime="3 минуты"
+                likeCount={4}
+                isLiked={false}
+            />
+            <Card
+                category="Front-end"
+                point="·"
+                date="1 месяц назад"
+                title="Как работать с CSS Grid"
+                description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+                readTime="3 минуты"
+                likeCount={4}
+                isLiked={false}
+            />
+            <Card
+                category="Front-end"
+                point="·"
+                date="1 месяц назад"
+                title="Как работать с CSS Grid"
+                description="Грид-раскладка (CSS Grid Layout) представляет собой двумерную систему сеток в CSS. Гриды подойдут и для верстки основных областей страницы.."
+                readTime="3 минуты"
+                likeCount={4}
+                isLiked={false}
+            />
+            {/*<>*/}
+            {/*    <Button isActive={false} />*/}
+            {/*</>*/}
         </>
-    </>
-  )
+    )
 }
